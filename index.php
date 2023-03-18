7d980a7549c481482d1f670ea8842e557872494f
tmagula <124317312+tmagula@users.noreply.github.com>
2023-03-18 22:30:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sk-SK">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Trip 2023">
+    <meta name="keywords" content="Trip,Rakusko,Svajciarsko,Francuzko,Spanielsko,Portugalsko">
+    <meta name="author" content="Tomas Magula">
+    <link href="styly.css" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Trip 2023</title>
+</head>
+<body>
+<?php
+include('C:\Users\magul\Documents\VS\WA1\xampp-portable-windows-x64-8.2.0-0-VS16\xampp\htdocs\magul\Trip 2023\Parts\navigation.php');
+?>
+
+
+
+
+<footer>
+<p>©Tomáš Magula 2023</p>
+</footer>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
