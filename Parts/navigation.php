cbd8f92c6d543e9b2f4085cb1558701d91c7df4c
tmagula <124317312+tmagula@users.noreply.github.com>
2023-03-18 22:28:45 +0100

    Add files via upload
    Adding navigation

---
+++ Parts/navigation.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-sm navbar-dark">
+        <!--<a class="navbar-brand" href="#"><img class="navbar-brand" src="obrazky/zastrcka.gif" width="21" height="20" alt="brand"></a>-->
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class= "nav-link" href="index.php">Hlavná stránka</a></li>
+            <li class="nav-item"><a class= "nav-link" href="Subpages/itenerary.html">Itenerary</a></li>
+            <li class="nav-item"><a class= "nav-link" href="#blog">Blog</a></li>
+            <li class='nav-item'><a class= "nav-link" href='Subpages/zoznam.php'>Zoznam</a></li>
+            <li class="nav-item"><a class= "nav-link" href="galeria.html">Galéria</a></li>
+            <li class="nav-item"><a class= "nav-link" href="onas.html">O nás</a></li>
+        </ul>
+    </nav>
